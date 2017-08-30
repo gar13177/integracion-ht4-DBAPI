4eb45e42237e7f66b263fa66ec621aacd453f78d
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2017-08-30 16:37:33 -0600

    Add nombre and contrasena filter

--- dbapi/dbms/views.py
+++ dbapi/dbms/views.py
@@ -2,14 +2,26 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import django_filters.rest_framework
 from dbms.models import *
 from dbms.serializers import *
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+    def get_queryset(self):
+        params = self.request.query_params
+        data = Usuario.objects.all()
+        if 'nombre' in params:
+            data = data.filter(nombre=params['nombre'])
+
+        if 'contrasena' in params:
+            data = data.filter(contrasena=params['contrasena'])
+
+        return data
 
 class OrdenViewSet(viewsets.ModelViewSet):
     queryset = Orden.objects.all()
