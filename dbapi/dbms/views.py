d2818cb7030a92e1649a80972a61a7f98a8941b8
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2017-09-05 16:15:06 -0600

    Fix: Return not found when user not in DB

--- dbapi/dbms/views.py
+++ dbapi/dbms/views.py
@@ -4,10 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 import django_filters.rest_framework
 from django.shortcuts import get_object_or_404
+from django.http import Http404
 from dbms.models import *
 from dbms.serializers import *
 from rest_framework import viewsets, generics, status
 from rest_framework.response import Response
+from dbms.serializers import UsuarioSerializer
 
 
 class UsuarioViewSet(viewsets.ModelViewSet):
@@ -16,19 +18,20 @@ class UsuarioViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         params = self.request.query_params
+        data = Usuario.objects.all()
         if 'nombre' in params:
             data = get_object_or_404(Usuario, nombre=params['nombre'])
             if not (isinstance(data, Usuario)):
-                return data
+                return Response("No se encontró", status=status.HTTP_404_NOT_FOUND)
+            data = [data]
 
         if 'contrasena' in params:
             data = get_object_or_404(Usuario, nombre=params['nombre'], contrasena=params['contrasena'])
             if not (isinstance(data, Usuario)):
-                return data
+                return Response("No se encontró", status=status.HTTP_404_NOT_FOUND)
+            data = [data]
 
-        return [data]
-
-    print(queryset)
+        return data
 
 
 class OrdenViewSet(viewsets.ModelViewSet):
