d4f036cbe469d50b0a4a74d78cb5d791e68d2748
Kevin Garcia <gar13177@uvg.edu.gt>
2017-09-06 00:08:19 -0600

    actualización de filtros

--- dbapi/dbms/views.py
+++ dbapi/dbms/views.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-import django_filters.rest_framework
-from django.shortcuts import get_object_or_404
-from django.http import Http404
 from dbms.models import *
 from dbms.serializers import *
-from rest_framework import viewsets, generics, status
-from rest_framework.response import Response
-from dbms.serializers import UsuarioSerializer
+from rest_framework import viewsets
+from rest_framework.exceptions import NotFound, ParseError
 
 
 class UsuarioViewSet(viewsets.ModelViewSet):
@@ -17,21 +12,23 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     serializer_class = UsuarioSerializer
 
     def get_queryset(self):
-        params = self.request.query_params
-        data = Usuario.objects.all()
-        if 'nombre' in params:
-            data = get_object_or_404(Usuario, nombre=params['nombre'])
-            if not (isinstance(data, Usuario)):
-                return Response("No se encontró", status=status.HTTP_404_NOT_FOUND)
-            data = [data]
+        params = dict(self.request.query_params)
 
-        if 'contrasena' in params:
-            data = get_object_or_404(Usuario, nombre=params['nombre'], contrasena=params['contrasena'])
-            if not (isinstance(data, Usuario)):
-                return Response("No se encontró", status=status.HTTP_404_NOT_FOUND)
-            data = [data]
+        if not len(params):
+            return self.queryset
 
-        return data
+        n_params = dict()
+        for key, value in params.items():
+            n_params[key] = value[0]
+            
+        try:
+            usuario = Usuario.objects.filter(**n_params)
+        except Exception as e:
+            raise ParseError(detail=e.message)
+    
+        if len(usuario) == 0:
+            raise NotFound(detail="Usuario with params %s not found"%str(n_params))
+        return usuario
 
 
 class OrdenViewSet(viewsets.ModelViewSet):
